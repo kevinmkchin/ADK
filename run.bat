46e0e18e359ebcd4a0ccef36dc9f5d5280858a0f
Kevin Chin <44921110+kevinmkchin@users.noreply.github.com>
2021-12-01 14:41:43 -0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -3,9 +3,9 @@
 rem run with argument "vs" if you want to start visual studio to debug
 
 IF "%1"=="vs" (
-    devenv build\debug\ADK.exe
+    devenv cmake-build-debug\ADK.exe
 ) ELSE (
-    pushd build\debug\
-    START /D ..\..\data ADK.exe
+    pushd cmake-build-debug
+    START /D ..\data ADK.exe
     popd
 )
